ccb856f6682ecc5ad33a9ac54741b126724ce746
Rodrigo Antonio <ragsweb@gmail.com>
2018-01-14 22:25:45 -0200

    Ajuste no STDIN para funcionar no windows

--- src/ZMain.cpp
+++ src/ZMain.cpp
@@ -1,10 +1,12 @@
 #include "DebugProtocolServer.h"
 #include "DebugProtocolCallbacks.h"
 #include "ProtocolHandlers.h"
+#include <fcntl.h>
+#include <io.h>
 int main()
 {
+	int result = _setmode( _fileno( stdout ), _O_BINARY );
 	vscode_debug::DebugProtocolServer server;
-
 	vscode_debug::JSONOutput Out(std::cout, std::cerr);
 	vscode_debug::JSONRPCDispatcher Dispatcher(std::make_unique<vscode_debug::Handler>(Out));	
 	vscode_debug::DebugProtocolCallbacks Callbacks(server);
