52af87e535fd851444205697d95562664ce14c3b
Rodrigo Antonio <ragsweb@outlook.com>
2018-02-06 22:48:34 -0200

    Linux Compile Ajust

--- test/basic_protocol_test.cpp
+++ test/basic_protocol_test.cpp
@@ -2,7 +2,7 @@
 #if !defined( WIN32 )
     #define BOOST_TEST_DYN_LINK
 #endif
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE( la ) {
     BOOST_CHECK_EQUAL(1, 1);
